85c62508b0a11fdfb53f6f96c2decd8cd6e0da92
unknown <mmahdi.nasiri@yahoo.com>
2020-04-10 11:12:54 -0700

    first for read and write

---
+++ files.py
@@ -0,0 +1,9 @@
+# import telebot
+
+
+# bot = telebot.TeleBot(token='1064935072:AAEs8k05qJj3sGfhwfY_4fQfVYMn1ADXDnk')
+
+file = open("newfile.txt", "w+")
+
+file.write("helloo how are u")
+file.close()
