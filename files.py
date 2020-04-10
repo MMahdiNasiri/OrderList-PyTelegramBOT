ac4a36fe0fd4c6223b709d6ee2b92ab03c8cfe67
unknown <mmahdi.nasiri@yahoo.com>
2020-04-10 11:29:48 -0700

    first read & append

--- files.py
+++ files.py
@@ -2,8 +2,16 @@
 
 
 # bot = telebot.TeleBot(token='1064935072:AAEs8k05qJj3sGfhwfY_4fQfVYMn1ADXDnk')
+myDict = {
+    "1": "hello",
+    "2": "whats up",
+    "4": "how are you",
+    "5": "bye"
+}
 
-file = open("newfile.txt", "w+")
 
-file.write("helloo how are u")
+file = open("newfile.txt", "a+")
+text = file.read()
+file.write(myDict)
+print(text)
 file.close()
