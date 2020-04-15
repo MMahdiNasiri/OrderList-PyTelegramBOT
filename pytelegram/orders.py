92dea992dda5d02a34e5ea93c34ca5d17cdb431f
unknown <mmahdi.nasiri@yahoo.com>
2020-04-15 05:15:00 -0700

    fix some bug for keyboard

--- pytelegram/orders.py
+++ pytelegram/orders.py
@@ -2,7 +2,6 @@ import telebot
 import time
 import sys
 from telebot import types
-import keyboards
 
 
 bot = telebot.TeleBot(token = "1064935072:AAEs8k05qJj3sGfhwfY_4fQfVYMn1ADXDnk")
@@ -20,6 +19,30 @@ class orders:
         return self.tags.get(tag.lower(), 0)
 
 
+
+
+
+def productkeyboard():
+    proditem = {}
+    markup = types.ReplyKeyboardMarkup()
+    for x in range(len(product)):
+        proditem[x] = types.KeyboardButton(product[x])
+    finishitem = types.KeyboardButton('finish')
+    for x in range(0,len(product),3):
+        markup.row(proditem[x], proditem[x+1], proditem[x+2])
+    markup.row(finishitem)
+    return markup
+
+def numberkeyboard():
+    numbers = {}
+    markup = types.ReplyKeyboardMarkup()
+    for x in range(13):
+        numbers[x] = types.KeyboardButton(str(x))
+    backItem = types.KeyboardButton('back')
+    for x in range(1,13,3):
+        markup.row(numbers[x], numbers[x+1], numbers[x+2])
+    markup.row(backItem)
+    return markup
 
 
 
