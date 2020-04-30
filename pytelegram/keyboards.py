42574915cb841bc2bbc8281e4bc5bcbbbd127c28
unknown <mmahdi.nasiri@yahoo.com>
2020-04-30 10:10:01 -0700

    changing way add keys

--- pytelegram/keyboards.py
+++ pytelegram/keyboards.py
@@ -5,20 +5,20 @@ import telebot
 
 
 def productkeyboard(*product):
-    proditem = {}
-    markup = types.ReplyKeyboardMarkup()
-    for x in range(len(product)):
-        proditem[x] = types.KeyboardButton(product[x])
+    proditem = []
+    markup = types.ReplyKeyboardMarkup(row_width=3)
+    for x in product:
+        proditem.append(types.KeyboardButton(x))
     finishitem = types.KeyboardButton('finish')
-    for x in range(0,len(product),3):
-        markup.row(proditem[x], proditem[x+1], proditem[x+2])
-    markup.row(finishitem)
+
+    markup.add(*proditem)
+    markup.add(finishitem)
     return markup
 
 def numberkeyboard():
     numbers = []
     markup = types.ReplyKeyboardMarkup(row_width=3)
-    for x in range(13):
+    for x in range(1,13):
         numbers.append(types.KeyboardButton(str(x)))
 
     markup.add(*numbers)
